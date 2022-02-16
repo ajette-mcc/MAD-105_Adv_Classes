8714b49d81a96acd7ea35ffd7ef2a48e6c017fa9
Al Jette <ajette@students.mchenry.edu>
2022-02-16 14:55:22 -0600

    Advanced Classes Assignment - due 21-Feb-2021

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,20 @@
-// MAD-105 Advanced Classes Assignment due 21-Feb-2022
+// MAD-105 Inheritance Assignment due 28-Feb-2022
 
-fun main()  {
-        // Define 3 employees per Employee Construct
-        var emp1 = Employee("Jane Doe", PositionTitle.ADIMINISTRATION ,true, 27.00, 1 )
-        var emp2 = Employee("Larry Jackson", PositionTitle.PRODUCTION, false, 22.00, 2)
-        var emp3 = Employee("John Smith", PositionTitle.MAINTENANCE, false, 21.0, 3)
+fun main() {
+        // Define current rates (fixed values) and customer(s)
+        val currentCommercialRate: Double = 5.00
+        val currentResidentialRate:Double = 6.00
+        var cust1 = Customer("Jane", "555-Cust1", "1 Cust Street", 1000.00)
+        var cust2 = Commercial("Menards", currentCommercialRate, 3, "Bill", "555-Cust2", "2 Cust St", 2000.00)
+        var cust3 = Residential(currentResidentialRate, senior = true, "Fred", "555-Cust3", "3 Cust St", 3000.00)
+        var cust4 = Residential(currentResidentialRate, senior = false, "Barney", "555-Cust4", "4 Cust St", 3000.00)
 
-        // Calculate Pay for 3 employees and print their information for the week
-        emp1.caclulatePay(45.00)
-        emp2.caclulatePay(50.00)
-        emp3.caclulatePay(35.00)
+        // Other variables in main
+        var quote: Double = 0.00
+
+        cust2.calculate()
+        cust3.calculate()
+        quote = cust4.calculate()
+
+        println("Quote for ${cust4.customerName} is:  ${quote}   " + quote)
 }
--- src/position.kt
+++
@@ -1,8 +0,0 @@
-enum class PositionTitle(var PrintedTitle: String) {
-    ADIMINISTRATION("Admin"),
-    PRODUCTION("Production"),
-    SALES("Sales"),
-    MAINTENANCE("Maintenance"),
-    TECHNICAL("Techincal"),
-    SECRETARIAL("Secretarial");
-}
